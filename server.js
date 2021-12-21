30d887a7f9781f2c4f306f89de5963465ac0cca6
Stephane Bilodeau <sbilodeau@users.noreply.github.com>
2021-12-21 17:07:49 -0500

    Set base_path from ENV vars (#22)

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 'use strict';
 
 // CREATE HTTP SERVER AND PROXY
+const path     = require('path');
+const express  = require('express');
+const app      = express();
+const basePath = path.join('/', process.env.BASE_PATH||'/', '/');
 
-var app = require('express')();
+if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING default`);
 
 var httpProxy = require('http-proxy');
 
@@ -17,15 +21,19 @@ var proxy = httpProxy.createProxyServer({});
 
 // CONFIGURE /APP/* ROUTES
 
-app.use('/app',   require('serve-static')(__dirname + '/app_build'));
-app.use('/app',   require('serve-static')(__dirname + '/app'));
-app.all('/app/*', function(req, res) { res.status(404).send(); } );
+const appRoutes = new express.Router();
 
-// CONFIGURE TEMPLATE
-
-app.get('/*', function (req, res) { res.render('template', { baseUrl: req.headers.base_url || '/' }); });
+appRoutes.use('/app',   require('serve-static')(__dirname + '/app_build'));
+appRoutes.use('/app',   require('serve-static')(__dirname + '/app'));
+appRoutes.all('/app/*', (req, res) => res.status(404).send());
+appRoutes.get('/*',     (req, res) => res.render('template', { baseUrl: basePath }));
 
 // START SERVER
+
+app.use(basePath, appRoutes);
+
+if(basePath!='/') app.use('/', appRoutes); // temps support for transition to Traefik 2 / AWS ALB
+
 
 app.listen(app.get('port'), function () {
 	console.log('Server listening on %j', this.address());
