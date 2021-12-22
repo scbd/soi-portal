a801890c172852c2768430e7e727b173e87113dd
Stephane <bilodeaustephane@gmail.com>
2021-12-22 10:23:46 -0500

    Use `base_url` http header (rollback) when no BASE_PATH env var provided

--- server.js
+++ server.js
@@ -5,9 +5,9 @@
 const path     = require('path');
 const express  = require('express');
 const app      = express();
-const basePath = path.join('/', process.env.BASE_PATH||'/', '/');
+const basePath = process.env.BASE_PATH ? toBasePath(process.env.BASE_PATH) : null;
 
-if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING default`);
+if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING 'base_url' http header or '/' as default`);
 
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
@@ -23,14 +23,24 @@ const appRoutes = new express.Router();
 appRoutes.use('/app',   require('serve-static')(__dirname + '/app_build'));
 appRoutes.use('/app',   require('serve-static')(__dirname + '/app'));
 appRoutes.all('/app/*', (req, res) => res.status(404).send());
-appRoutes.get('/*',     (req, res) => res.render('template', { baseUrl: basePath }));
+appRoutes.get('/*',     (req, res) => { 
+    const baseUrl = toBasePath(basePath || req.headers.base_url);
+    res.render('template', { baseUrl: urlSafe(baseUrl) })
+});
 
 // START SERVER
 
-app.use(basePath, appRoutes);
-
-if(basePath!='/') app.use('/', appRoutes); // temps support for transition to Traefik 2 / AWS ALB
+app.use(toBasePath(basePath), appRoutes);
 
 app.listen(app.get('port'), function () {
 	console.log('Server listening on %j', this.address());
 });
+
+function toBasePath(dir) {
+    return path.join('/', dir || '/', '/');
+}
+
+function urlSafe(dir) {
+    var parts = dir.split('/').map(o=>encodeURIComponent(o));
+    return path.join.apply(null, parts.map(o=>o||'/'));
+}
